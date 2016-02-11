f0cf02abc2c31b64bb5eda09690d728db71ae5bb
miletskiy <s.miletskiy@gmail.com>
2016-02-11 18:42:23 +0200

    ticket:1. Code view with hard data. Make tests pass for deploy.Tests [OK] flake8 [OK]

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -8,4 +8,16 @@ def contacts(request):
     """
     View for main page
     """
-    return render(request, 'contacts.html')
+
+    applicant = {
+            'first_name': 'John',
+            'last_name': 'Galt',
+            'email': 'john.galt@gmail.com',
+            'jabber': 'john.galt@khavr.com',
+            'skype': 'john.galt',
+            'birthday': '05/02/1879',
+            'bio': 'Some bio',
+            'contacts': 'Some other contacts'
+    }
+
+    return render(request, 'contacts.html', {'applicant': applicant})
