35c7e187b7a050d5055c69afb1ab2371d81922c3
miletskiy <s.miletskiy@gmail.com>
2016-02-11 16:09:07 +0200

    ticket:1. Test for main view. Contacts template. Hello urls. tests [fail]

---
+++ apps/hello/tests/test_views.py
@@ -0,0 +1,26 @@
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from ..views import contacts
+
+
+class ContactPageTest(TestCase):
+    """
+    Test for view contacts
+    """
+
+    def test_contacts_page_renders_correct_response(self):
+        """
+        Contacts view uses correct template for main page
+        """
+        response = self.client.get(reverse('hello:contacts'))
+        self.assertTemplateUsed(response, 'contacts.html')
+
+    def test_contacts_view_get_request(self):
+        """
+        Check for answer from server
+        """
+        response = self.client.get(reverse('hello:contacts'))
+        self.assertEqual(response.status_code, 200)
+
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,11 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def contacts(request):
+    """
+    View for main page
+    """
+    pass
