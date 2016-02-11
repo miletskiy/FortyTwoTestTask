189cd39b150a3a0771398d0581bd2529ce27c63b
miletskiy <s.miletskiy@gmail.com>
2016-02-11 18:10:26 +0200

    ticket:1. Add empty model + test view for hardcoded data. Tests [fail]

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Applicant(models.Model):
+    """Model for Applicant """
+
+    class Meta:
+        verbose_name = "Applicant"
+        verbose_name_plural = "Applicants"
+
+    def __str__(self):
+        pass
--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -2,6 +2,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from ..models import Applicant
+
 
 class ContactPageTest(TestCase):
     """
@@ -22,3 +24,47 @@ class ContactPageTest(TestCase):
         response = self.client.get(reverse('hello:contacts'))
         self.assertEqual(response.status_code, 200)
 
+    def test_contacts_uses_right_model(self):
+        """
+        Check for correct instance in the template
+        """
+        response = self.client.get(reverse('hello:contacts'))
+        self.assertIsInstance(response.context['applicant'], Applicant)
+
+    def test_contacts_view_passes_hardcoded_data(self):
+        """
+        Check for contacts view passes Applicant data to template
+        """
+
+        response = self.client.get(reverse('hello:contacts'))
+
+        self.assertEqual(response.context['applicant'].first_name, 'John')
+        self.assertEqual(response.context['applicant'].last_name, 'Galt')
+        self.assertEqual(response.context['applicant'].birthday, '05/02/1879')
+        self.assertIn('john.galt@gmail.com', str(response.content))
+        self.assertIn('john.galt@khavr.com', str(response.content))
+        self.assertIn('Some other contacts', str(response.content))
+
+    def test_contacts_view_passes_correct_insatance(self):
+        """
+        Check the view passes correct instance to template
+        """
+        Applicant.objects.create(
+            first_name='John',
+            last_name='Galt',
+            email='john.galt@gmail.com',
+            jabber='john.galt@khavr.com',
+            skype='john.galt',
+            birthday='05/02/1879',
+            bio='Some bio',
+            contacts='Some other contacts'
+            )
+        applicant1 = Applicant.objects.first()
+        applicant2 = Applicant.objects.last()
+        applicants = Applicant.objects.all().count()
+
+        response = self.client.get(reverse('hello:contacts'))
+
+        self.assertEqual(applicants, 2)
+        self.assertEqual(response.context['applicant'], applicant1)
+        self.assertNotEqual(response.context['applicant'], applicant2)
