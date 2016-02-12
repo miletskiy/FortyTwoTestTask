cc8dd4b7f5176c43137854a522aae9a8794550b1
miletskiy <s.miletskiy@gmail.com>
2016-02-12 11:09:44 +0200

    ticket:1. Write model applicant. Add migretions. FT [fail] flake8 [OK]

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -10,5 +10,45 @@ class Applicant(models.Model):
         verbose_name = "Applicant"
         verbose_name_plural = "Applicants"
 
+    first_name = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name='Name'
+    )
+    last_name = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name='Last name'
+    )
+    birthday = models.DateField(
+        blank=False,
+        null=True,
+        verbose_name='Birthday',
+        help_text='Please use the following format: <em>YYYY-MM-DD</em>'
+    )
+    bio = models.TextField(
+        blank=True,
+        verbose_name='Bio'
+    )
+    email = models.EmailField(
+        max_length=50,
+        blank=False,
+        verbose_name='Email'
+    )
+    jabber = models.EmailField(
+        max_length=50,
+        blank=True,
+        verbose_name='Jabber'
+    )
+    skype = models.CharField(
+        max_length=50,
+        blank=True,
+        verbose_name='Skype'
+    )
+    contacts = models.TextField(
+        blank=True,
+        verbose_name='Other contacts'
+    )
+
     def __str__(self):
-        pass
+        return '{} {}'.format(self.first_name, self.last_name)
