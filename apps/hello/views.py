9b2602846788ab49fefb49f1fcb7b4f8ff538104
miletskiy <s.miletskiy@gmail.com>
2016-02-15 14:20:15 +0200

    ticket:3. Turn html mockup into django template with hard-coded data. Tests[OK]

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Applicant
+import datetime
 
 
 def contacts(request):
@@ -19,5 +20,11 @@ def requests_list(request):
     """
     View for requests page
     """
+    requests = []
+    for i in range(10):
+        loq = request.GET.copy()
+        loq['time'] = datetime.datetime.now()
+        request.GET = loq
+        requests.append(request)
 
-    return render(request, 'requests_list.html')
+    return render(request, 'requests_list.html', {'requests': requests})
