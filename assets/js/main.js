1ea8ec9ad1765ac50f5bd54b535bb8e691590c42
miletskiy <s.miletskiy@gmail.com>
2016-02-16 12:12:04 +0200

    ticket:3. Code js for update/reset tab title. Tests[OK]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,36 @@
 
+var $pageTitle = $('title').text();
+var windowIsActive;
+
+$(window).focus(function (){
+    windowIsActive = true;
+});
+
+$(window).blur(function (){
+    windowIsActive = false;
+});
+
+
 function initRequests () {
     var $oldTable = $('#id_requests_table');
+    var $lastRequestOnPage = $oldTable.find('tr td:first()').html()
+    var currentUrl = location.href;
 
     $.ajax({
         type: 'GET',
-        url: '/requests/',
+        url: currentUrl,
         success: function(data, status, xhr){
             var $newTable = $(data).find('#id_requests_table');
-            if ($newTable.html() !== $oldTable.html()) {
-                $oldTable.html($newTable.html());
+            var $lastRequestInDb = $newTable.find('tr td:first()').html()
+            var diff = $lastRequestInDb - $lastRequestOnPage;
+            if (diff) {
+                if (windowIsActive) {
+                    $oldTable.html($newTable.html());
+                    $('title').text($pageTitle);
+                } else {
+                    var newTitle = '('+ diff +') '+ $pageTitle;
+                    $('title').text(newTitle);
+                };
             };
         },
         error: function(){
