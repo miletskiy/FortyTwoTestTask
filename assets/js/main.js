a166f44665923c54b71050c3a91a00a73965a838
miletskiy <s.miletskiy@gmail.com>
2016-02-24 17:34:40 +0200

    ticket:13. For sorted requests list fix updating page. Tests [OK]

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,20 +17,27 @@ var dateOptions = {
   hour:'2-digit',
   minute:'2-digit',
 };
+var oldMax;
+var newMax;
+var diff=0;
 
 function initRequests () {
     var $oldTable = $('#id_tbody');
-    var $lastRequestOnPage = $oldTable.find('td:first()').html()
     var currentUrl = location.href;
 
     $.ajax({
         type: 'GET',
         url: currentUrl,
         success: function(data, status, xhr){
+            if (!diff) {
+                oldMax = newMax;
+            };
+
+            var maxArray = [];
             var newTable;
             for (var i = 0; i < data.length; i++) {
-                var lastRequestInDb = data[0].pk;
                 var date = new Date(data[i].fields.emergence);
+                maxArray.push(data[i].pk);
                 var tableRow = '<tr><td>' + data[i].pk + '</td>';
                 tableRow += '<td>' + data[i].fields.title + '</td>';
                 tableRow += '<td>' + date.toLocaleString("en-US", dateOptions) + '</td>';
@@ -41,11 +48,18 @@ function initRequests () {
                 tableRow += '<td>' + data[i].fields.priority + '</td></tr>';
                 newTable +=tableRow;
             };
-            var diff = lastRequestInDb - $lastRequestOnPage;
+            newMax = Math.max(...maxArray);
+
+            if (newMax>oldMax) {
+                diff = newMax - oldMax;
+            };
+
             if (diff) {
                 if (windowIsActive) {
                     $oldTable.html(newTable);
                     $('title').text($pageTitle);
+                    oldMax = newMax;
+                    diff = 0;
                 } else {
                     var newTitle = '('+ diff +') '+ $pageTitle;
                     $('title').text(newTitle);
